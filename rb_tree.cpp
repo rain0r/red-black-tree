10a8c360f244ec5ccbbee492dfc33ba36f44982b
rain0r <rainer@hihn.org>
2014-05-04 12:39:42 +0200

    "working" version

--- rb_tree.cpp
+++ rb_tree.cpp
@@ -1,19 +1,42 @@
-#include <iostream>
-#include <iomanip>
 #include "rb_tree.h"
 
+void insert(Tree *t) {
+  for (int i = 0; i < 19; i++) {
+    int x = rand() % 10;
+    if (x > 0) {
+      t->insertValue(x);
+    }
+  }
+}
+
 int main(int argc, char* argv[]) {
-  std::cout << "Red Black Tree" << std::endl;
-  Tree t;
-  t.insertValue(1);
-  t.insertValue(2);
-  // insertValue(3);
-  // t.insertValue(4);
-  // t.insertValue(5);
-
-  // std::cout << "Count of Nodes: " << t.countNodes(t.getRoot()) << std::endl;
-
   std::cout << std::endl;
+  std::cout << "Red Black Tree" << std::endl;
+
+  boost::thread_group tree_threads;
+
+  int tcount = 4;
+  Tree t;
+  int nums[] = { 7, 9, 5, 2, 1, 6, 3, 8, 4 };
+
+  for (int i = 0; i != tcount; i++) {
+    boost::thread *t1 = new boost::thread(insert, &t);
+    tree_threads.add_thread(t1);
+  }
+
+  tree_threads.join_all();
+
+  t.print();
+
+//  bool b = t.deleteValue(4);
+//  if (b) {
+//    std::cout << "Success";
+//  } else {
+//    std::cout << "Fail";
+//  }
+//  std::cout << std::endl;
+//
+//  t.print();
 
   return 0;
 }
--- rb_tree.h
+++ rb_tree.h
@@ -1,6 +1,12 @@
 #ifndef C_BBTREE_H
 #define C_BBTREE_H
 
+#include <stdio.h>
+#include <boost/thread.hpp>
+#include <iostream>
+#include <iomanip>
+
+
 class Tree {
  private:
   enum Color {
@@ -14,13 +20,21 @@ class Tree {
     int value;
     bool isNil;
 
-    explicit Node(int value)
+    explicit Node(int value_)
         : parent(NULL),
           left(NULL),
           right(NULL),
           color(RED),
-          value(value),
+          value(value_),
           isNil(false) {
+    }
+
+    ~Node() {
+      parent = NULL;
+      left = NULL;
+      right = NULL;
+      value = 0;
+      color = BLACK;
     }
 
     Node* grandparent() const {
@@ -57,31 +71,141 @@ class Tree {
   }
 
   Node* rotateLeft(Node *n) {
+    Node *tmpNode = n->right;
+    n->right = tmpNode->left;
 
+    if (!tmpNode->left->isNil) {
+      tmpNode->left->parent = n;
+    }
+    tmpNode->parent = n->parent;
+    if (n->parent != NULL) {
+      if (n->parent->isNil) {
+        this->rootPtr = tmpNode;
+      }
+    } else if (n->parent->left == n) {
+      n->parent->left = tmpNode;
+    } else {
+      n->parent->right = tmpNode;
+    }
+    tmpNode->left = n;
+    n->parent = tmpNode;
+    return n;
   }
 
-  Node* rotateRight(Node *n) {
-
+  Node* rotateRight(Node *node) {
+    Node *tmpNode = node->left;
+    node->left = tmpNode->right;
+    if (!tmpNode->right->isNil) {
+      tmpNode->right->parent = node;
+    }
+    tmpNode->parent = node->parent;
+    if (node->parent != NULL) {
+      if (node->parent->isNil) {
+        this->rootPtr = tmpNode;
+      }
+    } else if (node->parent->left == node) {
+      node->parent->left = tmpNode;
+    } else {
+      node->parent->right = tmpNode;
+    }
+    tmpNode->right = node;
+    node->parent = tmpNode;
   }
 
-  Node *rootPtr = NULL;
-
-  void createNilLeaf(Node *parent) {
-    Node n = Node(-1);
-    n.parent = parent;
-    n.color = BLACK;
-    n.isNil = true;
+  void insert_case1(Node *node) {
+    if (node == NULL) {
+      return;
+    }
+    if (node->parent == NULL) {
+      node->color = BLACK;
+      return;
+    } else {
+      insert_case2(node);
+    }
   }
+  void insert_case2(Node *node) {
+    if (node == NULL) {
+      return;
+    }
+    if (node->parent->color == BLACK) {
+      return;
+    } else {
+      insert_case3(node);
+    }
+  }
+  void insert_case3(Node *node) {
+    if (node == NULL) {
+      return;
+    }
+    if ((node->uncle()) && (node->uncle()->color == RED)) {
+      node->parent->color = BLACK;
+      node->uncle()->color = BLACK;
+      node->grandparent()->color = RED;
+      insert_case1(node->grandparent());
+    } else {
+      insert_case4(node);
+    }
+  }
+  void insert_case4(Node *node) {
+    if (node == NULL) {
+      return;
+    }
+    if ((node == node->parent->right)
+        && (node->parent == node->grandparent()->left)) {
+      this->rotateLeft(node->parent);
+      node = node->left;
+    } else if (node == node->parent->left
+        && node->parent == node->grandparent()->right) {
+      this->rotateRight(node->parent);
+      node = node->right;
+    }
+    insert_case5(node);
+  }
+  void insert_case5(Node *node) {
+//    std::cout << "insert_case5(" << node->value << ")" << std::endl;
+    if (node == NULL) {
+      return;
+    }
+    node->parent->color = BLACK;
+    node->grandparent()->color = RED;
+    if (node == node->parent->left
+        && node->parent == node->grandparent()->left) {
+      this->rotateRight(node->grandparent());
+    } else {
+      this->rotateLeft(node->grandparent());
+    }
+  }
+
+  Node *rootPtr;
+  int nodeCounter;
+  boost::mutex mtx;
 
  public:
   Tree()
-//      : rootPtr(NULL)
- {
+      : rootPtr(NULL) {
     std::cout << "Tree()" << std::endl;
+    this->nodeCounter = 0;
   }
 
-  Node* getRoot() {
-    return this->rootPtr;
+  Node * getRootPtr() {
+    if (this->rootPtr != NULL) {
+      return this->rootPtr;
+    } else {
+      return NULL;
+    }
+  }
+
+  void createNilLeaves(Node* node) {
+    // Create 2 Nil Leaves
+    Node* nil_lft = new Node(NULL);
+    nil_lft->color = BLACK;
+    nil_lft->isNil = true;
+    Node* nil_rght = new Node(NULL);
+    nil_rght->color = BLACK;
+    nil_rght->isNil = true;
+
+    node->left = nil_lft;
+    node->right = nil_rght;
   }
 
   bool isEmpty() const {
@@ -92,114 +216,267 @@ class Tree {
   bool insertValue(int value) {
     std::cout << "insertValue(" << value << ")" << std::endl;
 
-    // check if the value to insert already exists
-    if (this->search(value) != NULL) {
-      return false;
-    }
-
-    if (this->rootPtr != NULL) {
-      std::cout << "Value of root Node: " << this->rootPtr->value << std::endl;
-    }
-
-    Node newNode = Node(value);
-
-    if (this->rootPtr != NULL) {
-      std::cout << "Value of root Node: " << this->rootPtr->value << std::endl;
-    }
-
+    this->mtx.lock();
     if (this->isEmpty()) {
       // Insert the first node of the tree
       // (root node)
       std::cout << "Adding root Node" << std::endl;
-      newNode.color = BLACK;
-      this->rootPtr = &newNode;
-
-      // Create 2 NIL-Leaves
-      for (int i = 0; i < 2; i++) {
-        this->createNilLeaf(&newNode);
-      }
+      Node* newNode = new Node(value);
+      newNode->color = BLACK;
+      this->rootPtr = newNode;
+      this->createNilLeaves(this->rootPtr);
+      this->nodeCounter += 1;
     } else {
+      // check if the value to insert already exists
+      if (this->search(value) != NULL) {
+        std::cout << "Value " << value << " already exists" << std::endl;
+        return false;
+      }
+
       // The new Node is not the Root
+      this->mtx.lock();
       if (this->addNewNode(this->rootPtr, value)) {
-        std::cout << "Added new Node with value: " << value << std::endl;
+//        std::cout << "Added new Node with value: " << value << std::endl;
+        this->nodeCounter += 1;
       } else {
-        std::cout << "Error adding new Node with value: " << value << std::endl;
+//        std::cout << "Error adding new Node with value: " << value << std::endl;
+        return false;
       }
     }
+    this->mtx.unlock();
 
-    // this->postorder(this->rootPtr, 2);
     return true;
   }
 
   Node* search(int value) {
 //    std::cout << "search(" << value << ")" << std::endl;
+    return this->searchWalk(this->getRootPtr(), value);
+  }
+
+  Node* searchWalk(Node *node, int value) {
+    if (node != NULL) {
+      if (node->value == value) {
+        return node;
+      } else {
+        if (value < node->value) {
+          return searchWalk(node->left, value);
+        } else {
+          return searchWalk(node->right, value);
+        }
+      }
+    }
     return NULL;
   }
 
   bool deleteValue(int value) {
-    std::cout << "deleteValue(" << value << ")" << std::endl;
+//    std::cout << "deleteValue(" << value << ")" << std::endl;
+    this->mtx.lock();
+    Node *toDelete = this->search(value);
+
+    if (toDelete == NULL) {
+      return false;
+    } else {
+      this->deleteOneChild(toDelete);
+      return true;
+    }
+    this->mtx.unlock();
+
     return false;
   }
 
-  // walk through the tree and find the correct spot to insert the new node
+  bool isLeaf(Node *node) {
+    if (node != NULL) {
+      if (node->right != NULL && node->left != NULL) {
+        if (node->right->isNil && node->left->isNil) {
+          return true;
+        }
+      }
+    }
+    return false;
+  }
+
+  void replaceNode(Node *node1, Node *node2) {
+    // node2 replaces node1
+    node1 = node2;
+    delete node2;
+  }
+
+  void delete_case1(Node *node) {
+    if (node->parent != NULL) {
+      delete_case2(node);
+    }
+  }
+
+  void delete_case2(Node *node) {
+    Node *s = node->sibling();
+
+    if (s->color == RED) {
+      node->parent->color = RED;
+      s->color = BLACK;
+      if (node == node->parent->left) {
+        rotateLeft(node->parent);
+      } else {
+        rotateRight(node->parent);
+      }
+    }
+    delete_case3(node);
+  }
+
+  void delete_case3(Node *node) {
+    Node *s = node->sibling();
+
+    if (node == NULL || node->parent == NULL || s == NULL || s->left == NULL
+        || s->right == NULL) {
+      return;
+    }
+
+    if ((node->parent->color == BLACK) && (s->color == BLACK)
+        && (s->left->color == BLACK) && (s->right->color == BLACK)) {
+      s->color = RED;
+      delete_case1(node->parent);
+    } else
+      delete_case4(node);
+  }
+
+  void delete_case4(Node *node) {
+    Node *s = node->sibling();
+
+    if ((node->parent->color == RED) && (s->color == BLACK)
+        && (s->left->color == BLACK) && (s->right->color == BLACK)) {
+      s->color = RED;
+      node->parent->color = BLACK;
+    } else
+      delete_case5(node);
+  }
+
+  void delete_case5(Node *node) {
+    Node *s = node->sibling();
+
+    if (s->color == BLACK) {
+      if ((node == node->parent->left) && (s->right->color == BLACK)
+          && (s->left->color == RED)) {
+        s->color = RED;
+        s->left->color = BLACK;
+        rotateRight(s);
+      } else if ((node == node->parent->right) && (s->left->color == BLACK)
+          && (s->right->color == RED)) {
+        s->color = RED;
+        s->right->color = BLACK;
+        rotateLeft(s);
+      }
+    }
+    delete_case6(node);
+  }
+
+  void delete_case6(Node *node) {
+    Node *s = node->sibling();
+
+    s->color = node->parent->color;
+    node->parent->color = BLACK;
+
+    if (node == node->parent->left) {
+      s->right->color = BLACK;
+      rotateLeft(node->parent);
+    } else {
+      s->left->color = BLACK;
+      rotateRight(node->parent);
+    }
+  }
+
+  void deleteOneChild(Node *node) {
+    Node *child = this->isLeaf(node->right) ? node->left : node->right;
+
+    this->replaceNode(node, child);
+    if (node->color == BLACK) {
+      if (child->color == RED) {
+        child->color = BLACK;
+      } else {
+        delete_case1(child);
+      }
+    }
+
+//    std::cout << "deleting :" << node->value << std::endl;
+    delete node;
+    node = NULL;
+  }
+
+// walk through the tree and find the correct spot to insert the new node
   bool addNewNode(Node *node, int value) {
-    std::cout << "addNewNode(" << value << ")" << std::endl;
+//    std::cout << "addNewNode(" << value << ")" << std::endl;
 
     if (node->value == value) {
+//      std::cout << "Error:" << node->value << "==" << value << std::endl;
       return false;
     } else if (value < node->value) {
-      std::cout << value << " < " << node->value << std::endl;
+//      std::cout << value << " < " << node->value << std::endl;
       // go left
-      if (node->left == NULL) {
+      if (node->left->isNil) {
+//        std::cout << "Left one is a NilNode" << std::endl;
         // Create new Node
-        Node newNode = Node(value);
-        node->left = &newNode;
+        Node* newNode = new Node(value);
+        node->left = newNode;
+        newNode->parent = node;
+        this->createNilLeaves(newNode);
+        //handle Color
+        this->insert_case1(newNode);
         return true;
       } else {
-        this->addNewNode(node->left, value);
+        return this->addNewNode(node->left, value);
       }
     } else if (value > node->value) {
-      std::cout << value << " > " << node->value << std::endl;
-
+//      std::cout << value << " > " << node->value << std::endl;
       // go right
-      if (node->right == NULL) {
+      if (node->right->isNil) {
+//        std::cout << "Right one is a NilNode" << std::endl;
         // Create new Node
-        Node newNode = Node(value);
-        node->right = &newNode;
+        Node* newNode = new Node(value);
+        node->right = newNode;
+        newNode->parent = node;
+        this->createNilLeaves(newNode);
+        //handle Color
+        this->insert_case1(newNode);
         return true;
       } else {
-        this->addNewNode(node->right, value);
+        return this->addNewNode(node->right, value);
       }
     }
     return false;
   }
 
-  void postorder(Node* p, int indent = 0) {
-    //std::cout << "postorder()" << std::endl;
+  void printNodeCounter() {
+    std::cout << "Count of Nodes: " << this->nodeCounter << std::endl;
+  }
 
-    if (p != NULL) {
-      if (p->left)
-        postorder(p->left, indent + 4);
-      if (p->right)
-        postorder(p->right, indent + 4);
-      if (indent) {
-        std::cout << std::setw(indent) << ' ';
-      }
-      std::cout << p->value << std::endl;
+  void padding(char ch, int n) {
+    int i;
+    for (i = 0; i < n; i++)
+      putchar(ch);
+  }
+
+  void structure(Node *root, int level) {
+    int i;
+
+    if (root == NULL) {
+      padding('\t', level);
+      puts("~");
+    } else {
+      this->structure(root->right, level + 1);
+      padding('\t', level);
+      char color = (root->color) ? 'B' : 'R';
+      std::cout << root->value << "(" << color << ")" << std::endl;
+      this->structure(root->left, level + 1);
     }
   }
 
-  int countNodes(Node *root) {
-    if (root == NULL)
-      return 0;
-    else {
-      int count = 1;
-      count += countNodes(root->left);
-      count += countNodes(root->right);
-      return count;
-    }
+  void print() {
+    std::cout << std::endl;
+    std::cout << std::endl;
+    this->structure(this->getRootPtr(), 0);
+    std::cout << std::endl;
+    std::cout << std::endl;
   }
 
-};
+}
+;
 
 #endif
